f3d0058ca11475261e865a98db0b3ad915fffbb1
zenos <zenos.game.anime.all.vo1@gmail.com>
2020-03-23 19:04:14 +0900

    action_text

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,29 @@
 class PostsController < ApplicationController
   def index
   end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
 end
