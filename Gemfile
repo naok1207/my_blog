6a536ed37f3434fcae89b555dd7843bd6bafa467
zenos <zenos.game.anime.all.vo1@gmail.com>
2020-03-25 06:27:41 +0900

    add jquery

--- Gemfile
+++ Gemfile
@@ -31,6 +31,9 @@ gem 'dotenv-rails'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', require: false
 
+gem 'will_paginate',           '3.1.7'
+# gem 'bootstrap-will_paginate', '1.0.0'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Post.create!(
+    title: 'ブログを始めました。',
+    content: '初めてのブログです。よろしくお願いします。。。。。。'
+)
+
+(2..20).each do |i|
+    Post.create!(
+        title: "#{i}つ目の記事です。",
+        content: "ブログって難しいぜ....<br /> 何も思いつかねぇ..."
+    )
+end
